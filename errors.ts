ebac260d35e88dc6c82fd288ed247a8d924d2e69
umajho <umajho@proton.me>
2023-03-01 12:56:39 +0800

    fix: 给 Unreachable 和 Unimplemented 加上 message

--- errors.ts
+++ errors.ts
@@ -4,9 +4,16 @@ export class Unreachable extends Error {
       super(message);
       return;
     }
-    super();
+    super("Unreachable");
   }
 }
 
 export class Unimplemented extends Error {
+  constructor(message?: string) {
+    if (message !== undefined) {
+      super(message);
+      return;
+    }
+    super("Unimplemented");
+  }
 }
