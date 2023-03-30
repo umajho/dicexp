9e13ad1ff2ad65d58fe9a29602e1a4575bd3f296
umajho <umajho@proton.me>
2023-03-30 15:18:04 +0800

    fix(parsing/test): 忘传参数

--- internal/parsing/test/parse.test.ts
+++ internal/parsing/test/parse.test.ts
@@ -284,7 +284,7 @@ function theyAreBad(
 ) {
   for (const [i, code] of table.entries()) {
     it(`case ${i + 1}: ${code}`, () => {
-      assertBad(code);
+      assertBad(code, parseFn);
     });
   }
 }
