70f383def164807e0d845163d9ac37803aa1f6ae
umajho <umajho@proton.me>
2023-03-31 13:13:56 +0800

    fix(playground): 错误信息未正确反映错误类型

--- internal/parsing/lib.ts
+++ internal/parsing/lib.ts
@@ -1,1 +1,2 @@
 export { parse, type ParseOptions, type ParsingResult } from "./src/parse";
+export { ParsingError } from "./src/parsing_error";
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -38,22 +38,18 @@
                 n-checkbox(v-model:checked="fixesSeed") 固定种子
               n-input-number(v-model:value="seed", :disabled="!fixesSeed", :precision="0")
         div(style="height: 40px")
-        div(v-if="result || otherError")
+        div(v-if="result")
           n-space(justify="center")
             n-card(style="width: 60vw; min-width: 600px")
               n-tabs(type="line")
                 n-tab-pane(name="result", tab="结果")
-                  template(v-if="otherError || result.error")
-                    template(v-if="otherError")
-                      n-alert(type="error", title="错误") {{ otherError.name }}
-                        code(style="white-space: pre") 
+                  template(v-if="result.error")
+                    n-alert(type="error", :title="`${errorDisplayInfo.kind}错误`")
+                      code(style="white-space: pre")
+                        | {{ result.error.message }}
+                        template(v-if="errorDisplayInfo.showsStack")
                           hr
-                          | {{ otherError.message }}
-                          hr
-                          | {{ otherError.stack }}
-                    template(v-else)
-                      n-alert(type="error", title="Dicexp 运行时错误")
-                        code(style="white-space: pre") {{ result.error.message }}
+                          | {{ result.error.stack }}
                   template(v-else)
                     code(style="white-space: pre-wrap") {{ JSON.stringify(result.ok) }}
                 n-tab-pane(v-if="result && result.representation", name="representation", tab="步骤展现（临时版本）")
@@ -64,6 +60,7 @@
 import { type MenuOption, NSkeleton, darkTheme /** used */ } from "naive-ui";
 
 import type { EvaluationResult, evaluate as evaluateFn } from "dicexp/internal";
+import { ParsingError, RuntimeError } from "dicexp/internal";
 
 const menuOptions: MenuOption[] = [
   {
@@ -110,7 +107,13 @@ const canRoll = computed(() => {
 });
 
 const result: Ref<EvaluationResult | null> = ref(null);
-const otherError: Ref<Error | null> = ref(null);
+const errorDisplayInfo = computed(() => {
+  if (!result.value || result.value.ok) return null;
+  const err = result.value.error!;
+  if (err instanceof ParsingError) return { kind: "解析", showsStack: false };
+  if (err instanceof RuntimeError) return { kind: "运行时", showsStack: false };
+  return { kind: "未知", showsStack: true };
+});
 
 function roll() {
   if (!canRoll) return;
@@ -119,16 +122,14 @@ function roll() {
     seed.value = crypto.getRandomValues(new Uint32Array(1))[0];
   }
 
-  otherError.value = null;
   result.value = null;
   try {
     result.value = evaluate.value!(code.value, { seed: seed.value });
   } catch (e) {
-    if (e instanceof Error) {
-      otherError.value = e;
-    } else {
-      otherError.value = new Error(`未知错误：${e}`);
+    if (!(e instanceof Error)) {
+      e = new Error(`未知抛出：${e}`);
     }
+    result.value = { error: e as Error, representation: null };
   }
 }
 
