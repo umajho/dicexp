b483b3a1b6b1d300d85c72d7be25e82371110b29
umajho <umajho@proton.me>
2023-04-04 16:33:04 +0800

    style(dicexp): 调整代码顺序

--- packages/dicexp/src/wokre_types.d.ts
+++ packages/dicexp/src/wokre_types.d.ts
@@ -4,14 +4,6 @@ export type DataToWorker =
   | [type: "initialize", init: WorkerInit]
   | [type: "evaluate", id: string, code: string, opts?: EvaluateOptions];
 
-export type InitializationResult =
-  | { ok: true; error?: never }
-  | { ok?: never; error: Error };
-
-export type EvaluatingSpecialErrorType =
-  | "parsing_error"
-  | "runtime_error";
-
 export type DataFromWorker =
   | [type: "initialize_result", result: InitializationResult]
   | [type: "heartbeat"]
@@ -28,3 +20,11 @@ export interface WorkerInit {
    */
   minHeartbeatInterval: { ms: number };
 }
+
+export type InitializationResult =
+  | { ok: true; error?: never }
+  | { ok?: never; error: Error };
+
+export type EvaluatingSpecialErrorType =
+  | "parsing_error"
+  | "runtime_error";
