5a26fb1bc79bbbec02ed119008354b2e0655cea8
umajho <umajho@proton.me>
2023-04-06 16:39:12 +0800

    fix(executing): 移除在 552b923 实现的优化，因为实现有问题
    而且性能提升也微乎其微。

--- internal/executing/src/random.ts
+++ internal/executing/src/random.ts
@@ -1,4 +1,4 @@
-import { Unimplemented, Unreachable } from "@dicexp/errors";
+import { Unimplemented } from "@dicexp/errors";
 
 export interface RandomSource {
   uint32(): number;
@@ -8,14 +8,6 @@ export class RandomGenerator {
   constructor(
     private readonly source: RandomSource,
   ) {}
-
-  remain = new Uint32Array(1);
-  remainBits = 0;
-
-  private _resetRemain() {
-    this.remain[0] = this.source.uint32();
-    this.remainBits = 32;
-  }
 
   /**
    * 闭区间。
@@ -38,39 +30,17 @@ export class RandomGenerator {
    * 0 至 n-1。
    */
   private _integerN32(n: number): number {
-    const requiredBits = getBits(n);
-
-    let usedBits = requiredBits;
-    let rn: number;
-
-    while (true) {
-      if (this.remainBits < usedBits) {
-        this._resetRemain();
-      }
-
-      if (usedBits === 32) {
-        rn = this.remain[0];
-      } else {
-        const mask = (1 << usedBits) - 1;
-        rn = this.remain[0] & mask;
-      }
-
-      const maxUnbiased = n <= 2
-        ? 2 ** this.remainBits - 1
-        : (2 ** this.remainBits / n | 0) * n - 1;
-
-      if (rn <= maxUnbiased) break;
-
-      if (usedBits === 32) {
-        usedBits = 0;
-        this._resetRemain();
-      } else {
-        usedBits++;
-      }
+    let maxUnbiased: number;
+    if (n <= 2) {
+      maxUnbiased = 2 ** 32 - 1;
+    } else {
+      maxUnbiased = (2 ** 32 / n | 0) * n - 1;
     }
 
-    this.remain[0] >>= usedBits;
-    this.remainBits -= usedBits;
+    let rn: number;
+    do {
+      rn = this.source.uint32();
+    } while (rn > maxUnbiased);
 
     return rn % n;
   }
@@ -91,13 +61,4 @@ export class RandomGenerator {
     } while (rn > maxUnbiased);
     return Number(rn % n);
   }
-}
-
-function getBits(n: number) {
-  if (!n) throw new Unreachable();
-  let count = 1;
-  for (n >>= 1; n; n >>= 1) {
-    count++;
-  }
-  return count;
 }
