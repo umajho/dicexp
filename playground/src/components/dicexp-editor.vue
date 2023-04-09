9e386637b6ecb959725032191460edc82fe7b941
umajho <umajho@proton.me>
2023-04-09 22:33:24 +0800

    feat(playground): 实现在空括号中间时，退格删除整个括号

--- playground/src/components/dicexp-editor.vue
+++ playground/src/components/dicexp-editor.vue
@@ -43,6 +43,32 @@ onMounted(() => {
       },
     },
   ]);
+  // FIXME: Codemirror 官网的 JavaScript 没有任何特殊处理就实现了此功能，问题出在哪里？
+  const removeSymmetryCloseBracket = EditorView.updateListener.of((update) => {
+    update.changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
+      // 若非仅是删除一个字符，退出处理
+      if (toB - fromB !== 0 || toA - fromA !== 1) return;
+
+      // 若删除的字符及之后的字符不是一组空的括号，退出处理
+      const pair = update.startState.doc.sliceString(fromA, toA + 1);
+      if (pair !== "()" && pair !== "[]") return;
+
+      // 若非只是一个光标，退出处理
+      const selection = update.startState.selection;
+      const mainCursor = selection.main;
+      const justMainCursor =
+        selection.ranges.length === 1 && mainCursor.from === mainCursor.to;
+      if (!justMainCursor) return;
+
+      // 若光标不在删除的开括号后侧，退出处理
+      if (mainCursor.from !== toA) return;
+
+      const tr = update.state.update({
+        changes: { from: fromB, to: fromB + 1, insert: "" },
+      });
+      view.dispatch(tr);
+    });
+  });
   const linting = linter((view) => {
     // 参考：https://discuss.codemirror.net/t/showing-syntax-errors/3111/6
     if (view.state.doc.line(1).text.trim() === "") return [];
@@ -74,6 +100,7 @@ onMounted(() => {
       sync,
       Prec.high(confirmByEnter),
       dicexp(),
+      removeSymmetryCloseBracket,
       linting,
     ],
   });
