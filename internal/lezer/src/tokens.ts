1b7f92acce2751344a31ac4a2c201910aa8ed3f9
umajho <umajho@proton.me>
2023-04-09 16:28:06 +0800

    refactor(lezer): 使用外部 tokenizer 处理标识符与 `d` 运算符

---
+++ internal/lezer/src/tokens.ts
@@ -0,0 +1,41 @@
+import { ExternalTokenizer } from "@lezer/lr";
+
+import { identifier, keywordD } from "./dicexp.grammar.term";
+
+const identifierStart = /[_a-zA-Z]/;
+const identifierContinue = /[_a-zA-Z0-9]/;
+
+const code_d = "d".charCodeAt(0);
+const code_0 = "0".charCodeAt(0);
+const code_9 = "9".charCodeAt(0);
+
+export const identifierTokenizer = new ExternalTokenizer((input, _stack) => {
+  if (input.next === -1) return;
+  const first = input.next;
+
+  if (first === code_d) {
+    input.advance();
+    if (input.next >= code_0 && input.next <= code_9) {
+      input.acceptToken(keywordD); // 投骰子的 “d” 运算符
+      return;
+    }
+  } else {
+    if (!identifierStart.test(String.fromCharCode(first))) return;
+    input.advance();
+  }
+
+  let singleChar = true;
+  while (
+    input.next !== -1 &&
+    identifierContinue.test(String.fromCharCode(input.next))
+  ) {
+    input.advance();
+    singleChar = false;
+  }
+
+  if (singleChar && first === code_d) {
+    input.acceptToken(keywordD); // 投骰子的 “d” 运算符
+  } else {
+    input.acceptToken(identifier);
+  }
+}, { contextual: true, fallback: true });
