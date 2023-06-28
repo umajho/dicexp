763b153f54b6597dc34d98566253bbc721028333
umajho <umajho@proton.me>
2023-06-28 21:36:17 +0800

    feat(playground): 为外部链接添加用于识别的图标

--- playground/src/components/layout/header.vue
+++ playground/src/components/layout/header.vue
@@ -5,13 +5,21 @@
   .flex-none
     ul.menu.menu-horizontal.px-1
       li
-        a(
+        a.inline-flex.items-center(
           href="https://github.com/umajho/dicexp/blob/main/docs/Dicexp.md",
           target="_blank"
-        ) 文档
+        )
+          | 文档
+          ArrowTopRightOnSquareIcon.w-4.h-4
       li
-        a(
+        a.inline-flex.items-center(
           href="https://github.com/umajho/dicexp",
           target="_blank"
-        ) 代码仓库
+        ) 
+          | 代码仓库
+          ArrowTopRightOnSquareIcon.w-4.h-4
 </template>
+
+<script setup lang="ts">
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/20/solid'
+</script> 
