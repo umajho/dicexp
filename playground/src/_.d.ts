d6e0b1e719a0b0f1d2dd7bc1133d4eb17fe593c7
umajho <umajho@proton.me>
2023-07-04 02:22:13 +0800

    refactor(playground): 删除旧 playground
    为了防止 git 误将删除+移动识别为修改文件的临时步骤。
    这一步肯定构建不了。

--- playground/src/_.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "esm-seedrandom" {
-  const prng_xorshift7: any;
-}
-
-declare module "vue-json-viewer";
--- playground/src/components/common/skeleton.vue
+++
@@ -1,3 +0,0 @@
-<template lang="pug">
-.w-full.h-full.bg-slate-700.animate-pulse
-</template>
