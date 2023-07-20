6fb2ea5146f91039ea00a0d7cca6d24949b81100
umajho <umajho@proton.me>
2023-07-20 15:04:36 +0800

    fix(executing/bench): 使 bench 重新可以运行
    依赖的先前版本的 vitest 存在 bug，导致 bench 无法执行。
    现在更新了 vitest 的版本，并暂时注释掉了其他目前无法执行的 dicexp 代码。

--- packages/dicexp/test/executing/execute.bench.ts
+++ packages/dicexp/test/executing/execute.bench.ts
@@ -24,13 +24,17 @@ describe("各种表达式", () => {
 
     // 模拟 if-else（现在已经不需要了），速度竟然差不多
     String.raw`append(filter([10], \(_ -> false)), 100) |> head`,
-    String.raw`if(false, 10, 100)`,
+    ...[ // FIXME: 重新实现 `if/*`
+      // String.raw`if(false, 10, 100)`,
+    ],
 
     // 0..<99
     String
       .raw`\($f, $n, $l -> append(filter([\( -> $l)], \(_ -> $n == 100)), \( -> $f.($f, $n+1, append($l, $n)))) |> head |> \($f -> $f.()).()) |> \($f -> $f.($f, 0, [])).()`,
-    String
-      .raw`\($f, $n, $l -> if($n == 100, $l, $f.($f, $n+1, append($l, $n)))) |> \($f -> $f.($f, 0, [])).()`,
+    ...[ // FIXME: 重新实现 `if/*`
+      // String
+      //   .raw`\($f, $n, $l -> if($n == 100, $l, $f.($f, $n+1, append($l, $n)))) |> \($f -> $f.($f, 0, [])).()`,
+    ],
 
     ...(() => {
       const yCombinators = [
@@ -45,13 +49,15 @@ describe("各种表达式", () => {
           // see: https://blog.klipse.tech/lambda/2016/08/10/pure-y-combinator-javascript.html
           String
             .raw`\($if -> \($Y, $g -> $Y.($g).(10)).(${yCombinator}, \($f -> \($n -> $if.($n == 0, \(-> 0), \(-> $n + $f.($n-1))))))).(\($cond, $t, $f -> head(append(filter([$t], \(_ -> $cond)), $f)).()))`,
-          // 用真正的 if-else：
-          String
-            .raw`\($Y, $g -> $Y.($g).(10)).(${yCombinator}, \($f -> \($n -> if($n == 0, 0, $n + $f.($n-1)))))`,
+          ...[ // FIXME: 重新实现 `if/*`，以及其中一项应该还会导致卡死
+            // // 用真正的 if-else：
+            // String
+            //   .raw`\($Y, $g -> $Y.($g).(10)).(${yCombinator}, \($f -> \($n -> if($n == 0, 0, $n + $f.($n-1)))))`,
 
-          // 0..<99，但是用 Y 组合子：
-          String
-            .raw`\($Y, $g -> $Y.($g).([0, []])).(${yCombinator}, \($f -> \($nl -> if(($nl|>at(0)) == 100, $nl|>at(1), $f.([($nl|>at(0))+1, append(($nl|>at(1)), $nl|>at(0))])))))`,
+            // // 0..<99，但是用 Y 组合子：
+            // String
+            //   .raw`\($Y, $g -> $Y.($g).([0, []])).(${yCombinator}, \($f -> \($nl -> if(($nl|>at(0)) == 100, $nl|>at(1), $f.([($nl|>at(0))+1, append(($nl|>at(1)), $nl|>at(0))])))))`,
+          ],
         ];
       });
     })(),
