5558adc0079c2babb2f5b693d9974279cadfd820
umajho <umajho@proton.me>
2023-09-18 18:49:30 +0800

    feat(solid-comp…/s…-repr…): 调整默认配色

--- packages/solid-components/src/steps-representation/color-scheme-default.ts
+++ packages/solid-components/src/steps-representation/color-scheme-default.ts
@@ -1,22 +1,40 @@
 import { ColorScheme } from "./color-scheme";
 
 export const defaultColorScheme: ColorScheme = {
+  // levels: [
+  //   [ // indigo-900 < blue > sky-900
+  //     { background: [0x31, 0x2e, 0x81] },
+  //     { background: [0x0c, 0x4a, 0x6e] },
+  //   ],
+  //   [ // emerald-900 < green > lime-900
+  //     { background: [0x06, 0x4e, 0x3b] },
+  //     { background: [0x36, 0x53, 0x14] },
+  //   ],
+  //   [ // fuchsia-900 < purple > violet-900
+  //     { background: [0x70, 0x1a, 0x75] },
+  //     { background: [0x4c, 0x1d, 0x95] },
+  //   ],
+  //   [ // amber-900 < orange > red-900
+  //     { background: [0x78, 0x35, 0x0f] },
+  //     { background: [0x7f, 0x1d, 0x1d] },
+  //   ],
+  // ],
   levels: [
-    [ // indigo-900 < blue > sky-900
-      { background: [0x31, 0x2e, 0x81] },
-      { background: [0x0c, 0x4a, 0x6e] },
+    [ // indigo-950 < blue > sky-950
+      { background: [0x1e, 0x1b, 0x4b] },
+      { background: [0x08, 0x2f, 0x49] },
     ],
-    [ // emerald-900 < green > lime-900
-      { background: [0x06, 0x4e, 0x3b] },
-      { background: [0x36, 0x53, 0x14] },
+    [ // emerald-950 < green > lime-950
+      { background: [0x02, 0x2c, 0x22] },
+      { background: [0x1a, 0x2e, 0x05] },
     ],
-    [ // fuchsia-900 < purple > violet-900
-      { background: [0x70, 0x1a, 0x75] },
-      { background: [0x4c, 0x1d, 0x95] },
+    [ // fuchsia-950 < purple > violet-950
+      { background: [0x4a, 0x04, 0x4e] },
+      { background: [0x2e, 0x10, 0x65] },
     ],
-    [ // amber-900 < orange > red-900
-      { background: [0x78, 0x35, 0x0f] },
-      { background: [0x7f, 0x1d, 0x1d] },
+    [ // amber-950 < orange > red-950
+      { background: [0x45, 0x1a, 0x03] },
+      { background: [0x45, 0x0a, 0x0a] },
     ],
   ],
 
