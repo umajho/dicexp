3c7284742375e778c357a8af727d7c2e002b896f
umajho <umajho@proton.me>
2023-10-11 12:41:05 +0800

    test(solid-components): 将范例测试用 describe.todo 包住，并并注释掉

--- packages/solid-components/test/server.test.tsx
+++ packages/solid-components/test/server.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
-import { isServer, renderToString } from "solid-js/web";
-import { createHello, Hello } from "../src";
+import { isServer /*, renderToString*/ } from "solid-js/web";
+// import { createHello, Hello } from "../src";
 
 describe("environment", () => {
   it("runs on server", () => {
@@ -9,22 +9,24 @@ describe("environment", () => {
   });
 });
 
-describe("createHello", () => {
-  it("Returns a Hello World signal", () => {
-    const [hello] = createHello();
-    expect(hello()).toBe("Hello World!");
-  });
+describe.todo(() => {
+  // describe("createHello", () => {
+  //   it("Returns a Hello World signal", () => {
+  //     const [hello] = createHello();
+  //     expect(hello()).toBe("Hello World!");
+  //   });
 
-  it("Changes the hello target", () => {
-    const [hello, setHello] = createHello();
-    setHello("Solid");
-    expect(hello()).toBe("Hello Solid!");
-  });
-});
+  //   it("Changes the hello target", () => {
+  //     const [hello, setHello] = createHello();
+  //     setHello("Solid");
+  //     expect(hello()).toBe("Hello Solid!");
+  //   });
+  // });
 
-describe("Hello", () => {
-  it("renders a hello component", () => {
-    const string = renderToString(() => <Hello />);
-    expect(string).toBe("<div>Hello World!</div>");
-  });
+  // describe("Hello", () => {
+  //   it("renders a hello component", () => {
+  //     const string = renderToString(() => <Hello />);
+  //     expect(string).toBe("<div>Hello World!</div>");
+  //   });
+  // });
 });
