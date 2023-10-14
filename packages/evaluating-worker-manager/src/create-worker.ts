37e9f50d73f284b3620caa2cdfd789a5ebe81dee
umajho <umajho@proton.me>
2023-10-14 23:41:49 +0800

    fix(evaluating-worker-manager): 修复作为依赖引入时无法找到 worker 的问题
    更具体一些：是在并非通过 "./internal" 入口来引入时。

--- packages/evaluating-worker-manager/src/create-worker.ts
+++ packages/evaluating-worker-manager/src/create-worker.ts
@@ -1,4 +1,6 @@
-import Worker from "./container.worker?worker";
+// 不 `&inline` 的话，构建后的本包作为依赖被引入后，worker 文件不会被 vite 分析到，
+// 从而导致错误。
+import Worker from "./container.worker?worker&inline";
 
 export function createWorkerByImportURLs(
   dicexpImportURL: string,
