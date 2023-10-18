9164fdd526656396a541e70309cae509a0211863
umajho <umajho@proton.me>
2023-10-18 14:14:43 +0800

    refactor(dicexp): 整理引出的类型

--- packages/dicexp/src/executing/mod.ts
+++ packages/dicexp/src/executing/mod.ts
@@ -1,9 +1,21 @@
-export { execute, type ExecuteOptions, type ExecutionResult } from "./execute";
-export type { ExecutionAppendix, JSValue } from "./runtime";
-export type { Restrictions as RuntimeRestrictions } from "./restrictions";
+export { execute } from "./execute";
+
+export type {
+  ExecuteOptions, // `execute` 的选项
+  ExecutionResult, // `execute` 返回的结果
+} from "./execute";
+export type {
+  ExecutionAppendix, // `ExecutionResult` 在 "ok" 及 "error" 时对应的结果的第二项
+  JSValue, // `ExecutionResult` 在 "ok" 时对应的结果的第一项
+  Statistics, // `ExecutionResult` 中的一项
+} from "./runtime";
+export type {
+  Restrictions as RuntimeRestrictions, // `ExecutionResult` 中的一项
+} from "./restrictions";
 export {
-  asRuntimeError,
-  type Repr,
-  type RuntimeError,
+  type Repr, // `ExecutionResult` 中的一项
+  type RuntimeError, // `ExecutionResult` 在 "error" 时对应的结果的第一项
 } from "@dicexp/runtime/values";
+
+export { asRuntimeError } from "@dicexp/runtime/values";
 export { asScope } from "@dicexp/runtime/regular-functions";
--- packages/dicexp/src/parsing/mod.ts
+++ packages/dicexp/src/parsing/mod.ts
@@ -1,2 +1,12 @@
-export { parse, type ParseOptions, type ParsingResult } from "./parse";
-export { ParsingError } from "./parsing_error";
+export { parse } from "./parse";
+
+export type {
+  ParseOptions, // `parse` 的选项
+  ParsingResult, // `parse` 返回的结果
+} from "./parse";
+export type {
+  Node, // `ParsingResult` 在 "ok" 时对应的结果
+} from "@dicexp/nodes";
+export type {
+  ParsingError, // `ParsingResult` 在 "error" 时对应的结果
+} from "./parsing_error";
