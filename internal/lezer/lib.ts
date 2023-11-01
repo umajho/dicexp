6407932a7b14cb56ac764c8482c3f010e6daebbc
umajho <umajho@proton.me>
2023-11-01 22:56:38 +0800

    fix(lezer): 引入路径没有更新导致 CI 测试失败

--- internal/lezer/lib.ts
+++ internal/lezer/lib.ts
@@ -1,2 +1,2 @@
-export { parser } from "./src/dicexp.grammar.js";
-export { precedenceTable } from "./src/precedence-table.js";
+export { parser } from "./src/dicexp.grammar";
+export { precedenceTable } from "./src/precedence-table";
