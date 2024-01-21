f55a9756ac60496229f23bb4909b9466e68ed0cc
umajho <umajho@proton.me>
2024-01-21 19:01:39 +0800

    fix(playground): 订正内部变量的名称

--- playground/src/workers/evaluation.worker.ts
+++ playground/src/workers/evaluation.worker.ts
@@ -1,10 +1,10 @@
 import { startWorkerServer } from "@dicexp/naive-evaluator-in-worker/internal";
 
-import { essenceForWorker as dicexp } from "@dicexp/naive-evaluator/internal";
+import { essenceForWorker as evaluatorMaker } from "@dicexp/naive-evaluator/internal";
 import { essenceOfBuiltinScope as builtinScope } from "@dicexp/naive-evaluator-builtins/internal";
 
 // // 测试 essences 用版本：
 // import dicexp from "dicexp/essence/for-worker?url";
 // import builtinScope from "@dicexp/naive-evaluator-builtins/essence/builtin-scope?url";
 
-startWorkerServer(dicexp, builtinScope);
+startWorkerServer(evaluatorMaker, builtinScope);
