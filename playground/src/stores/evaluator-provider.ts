d3f6d02ef0b74a2d2c506853152c5d69d295cb7a
umajho <umajho@proton.me>
2024-01-22 16:56:38 +0800

    style(playground): 让变量名更明确一些

--- playground/src/stores/evaluator-provider.ts
+++ playground/src/stores/evaluator-provider.ts
@@ -1,22 +1,22 @@
 import { DicexpEvaluatorProvider } from "@rotext/solid-components";
 
-import DicexpEvaluatingWorker from "../workers/evaluation.worker?worker";
+import NaiveEvaluatorWorker from "../workers/evaluation.worker?worker";
 
-let Manager:
+let NaiveEvaluatorWorkerManager:
   | typeof import("@dicexp/naive-evaluator-in-worker/internal").EvaluatingWorkerManager
   | null = null;
 
 export const defaultEvaluatorProvider = {
   default: (opts?: { readinessWatcher?: (ready: boolean) => void }) => {
     return new Promise(async (r) => {
-      if (!Manager) {
-        Manager = (await import(
+      if (!NaiveEvaluatorWorkerManager) {
+        NaiveEvaluatorWorkerManager = (await import(
           "@dicexp/naive-evaluator-in-worker/internal"
         )).EvaluatingWorkerManager;
       }
       let hasBeenReady = false;
-      const manager: any = new Manager(
-        () => new DicexpEvaluatingWorker(),
+      const manager: any = new NaiveEvaluatorWorkerManager(
+        () => new NaiveEvaluatorWorker(),
         (ready) => {
           if (ready && !hasBeenReady) {
             r(manager);
