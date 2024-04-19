f6f19240da6d6b3d3df0c14d561a02827294d569
umajho <umajho@proton.me>
2024-04-19 15:36:19 +0800

    fix(naive-evaluator-in-worker): 进一步放宽传入的求值器的类型
    先前的放宽还不够。

--- packages/naive-evaluator-in-worker/src/worker-inner/internal-types.ts
+++ packages/naive-evaluator-in-worker/src/worker-inner/internal-types.ts
@@ -1,3 +1,12 @@
-import { Evaluator } from "@dicexp/naive-evaluator";
+import type * as I from "@dicexp/interface";
+import { EvaluationResult, Evaluator } from "@dicexp/naive-evaluator";
 
-export interface NaiveEvaluator extends Evaluator {}
+/**
+ * XXX: 如果直接使用 "@dicexp/naive-evaluator" 中的 Evaluator 而不用这里的类型，
+ * 会导致外边在调用 startWorkerServer 时 tsc 报类型错误，表示期待的 Evaluator 与
+ * 传入的 Evaluator 之间私有属性的声明不同。
+ */
+export interface NaiveEvaluator extends I.Evaluator {
+  evaluate(code: string, opts: I.EvaluationOptions): EvaluationResult;
+}
+((x: Evaluator): NaiveEvaluator => x);
